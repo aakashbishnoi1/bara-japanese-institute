fe843530350c8c5fab808e0ac0768ffd906b8bb5
aakashbishnoi1 <8xaakash@gmail.com>
2024-01-13 10:22:56 +0530

    add layout to welcome container
    layout added to the container

--- Components/Welcome.tsx
+++ Components/Welcome.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Welcome = () => {
   return (
-    <div className="welcome">Welcome to my website </div>
+    <div className="welcome">
+        <div className="content">
+            <h1>Bara Japanese Language institute</h1>
+        </div>
+        <div className="welcome-pic">
+            <p>There will be a picture here</p>
+        </div>
+    </div>
   )
 }
 
